907e797cc2be638428ddb78d5084d996dd3dc641
Lawrence Macharia <macharialawrence2011@gmail.com>
2018-07-21 19:56:22 +0300

    initial commit

---
+++ app/src/main/java/com/mobidev/pickmeup/view/activities/PlaceActivity.kt
@@ -0,0 +1,12 @@
+package com.mobidev.pickmeup.view.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mobidev.pickmeup.R
+
+class PlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place)
+    }
+}

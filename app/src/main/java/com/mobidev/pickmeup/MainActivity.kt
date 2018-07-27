a49810b61b7d3a9e754ad99c88b54632b2dda2b4
Lawrence Macharia <macharialawrence2011@gmail.com>
2018-07-27 09:25:18 +0300

    - some logic to request location permissions then show current user location

--- app/src/main/java/com/mobidev/pickmeup/MainActivity.kt
+++ app/src/main/java/com/mobidev/pickmeup/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.mobidev.pickmeup
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.os.Build
 import android.os.Bundle
-
+import android.os.Looper
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, EasyPermissions.PermissionCallbacks {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mLastLocation: Location
+    private var mCurrLocationMarker: Marker? = null
+
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +46,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(LocationCallback())
     }
 
     /**
@@ -34,10 +64,132 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // location permission already granted
+                println("ocation permission already granted")
+                buildGoogleApiClient()
+                mMap.isMyLocationEnabled = true
+            } else {
+                // Request location permission
+                println("Request location permission")
+                checkLocationPermission()
+            }
+        } else {
+            buildGoogleApiClient()
+            mMap.isMyLocationEnabled = true
+        }
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    @Synchronized
+    private fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+        mGoogleApiClient.connect()
+    }
+
+
+    private fun checkLocationPermission() {
+//        val perms = Array<String>(1) { Manifest.permission.ACCESS_FINE_LOCATION }
+        val perms = Manifest.permission.ACCESS_FINE_LOCATION
+        if (!EasyPermissions.hasPermissions(this, perms.toString())) {
+            println("No permissions")
+
+            EasyPermissions.requestPermissions(this@MainActivity, "This app needs the Location permission, please accept to use location functionality",
+                    MY_PERMISSIONS_REQUEST_LOCATION, perms)
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+//                AlertDialog.Builder(this)
+//                        .setTitle("Location Permission Needed")
+//                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+//                        .setPositiveButton("OK", object : DialogInterface.OnClickListener {
+//                            override fun onClick(dialogInterface: DialogInterface?, i: Int) {
+//                                ActivityCompat.requestPermissions(this@MainActivity, perms, MY_PERMISSIONS_REQUEST_LOCATION)
+//                            }
+//                        })
+//                        .create()
+//                        .show()
+//            } else {
+//
+//            }
+        }
+
+        println("Has permissions")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, LocationCallback(), mainLooper)
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        mLastLocation = location!!
+        mCurrLocationMarker!!.remove()
+
+        // Place current location marker
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+        mCurrLocationMarker = mMap.addMarker(markerOptions)
+
+        // move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11F))
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                buildGoogleApiClient()
+                mMap.isMyLocationEnabled = true
+            }
+        }
     }
 }
